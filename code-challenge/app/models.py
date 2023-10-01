05f9002633daa82e92f2ec2b8cb42379f12fc46b
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-10-01 23:52:28 +0300

    update models.py

--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -3,8 +3,28 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Hero(db.Model):
-    __tablename__ = 'hero'
+  __tablename__ = 'hero'  
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(80), nullable=False)
+  secret_name = db.Column(db.String(80), nullable=False)
+  age = db.Column(db.Integer)
 
-    id = db.Column(db.Integer, primary_key=True)
+  powers = db.relationship('HeroPower', back_populates='hero')
 
-# add any models you may need. 
+class Power(db.Model):
+  __tablename__ = 'power'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(255), nullable=False)
+   
+  heroes = db.relationship('HeroPower', back_populates='power')  
+
+class HeroPower(db.Model):
+  __tablename__ = 'hero_power'
+  id = db.Column(db.Integer, primary_key=True)
+  strength = db.Column(db.Integer, nullable=False)
+
+  hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'))
+  power_id = db.Column(db.Integer, db.ForeignKey('power.id'))
+
+  hero = db.relationship('Hero', back_populates='powers')
+  power = db.relationship('Power', back_populates='heroes')
