31266cf4ce8101d5378b106811166a6ba91806dc
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-10-02 22:31:49 +0300

    initializing the db and migrations

--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -1,7 +1,26 @@
 from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
+
+class Power(db.Model):
+    __tablename__ = 'power'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False, unique=True)
+    description = db.Column(db.String(500), nullable=False)
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if len(name.strip()) == 0:
+            raise ValueError("Name must not be empty")
+        return name
+
+    @validates('description')
+    def validate_description(self, key, description):
+        if len(description.strip()) == 0:
+            raise ValueError("Description must not be empty")
+        return description
 
 class Hero(db.Model):
     __tablename__ = 'hero'
@@ -10,27 +29,17 @@ class Hero(db.Model):
     name = db.Column(db.String(255), nullable=False)
     super_name = db.Column(db.String(255), nullable=False)
 
-    # Define the relationship with HeroPower
-    powers = db.relationship('HeroPower', back_populates='hero')
-
-class Power(db.Model):
-    __tablename__ = 'power'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.Text, nullable=False)
-
-    # Define the relationship with HeroPower
-    heroes = db.relationship('HeroPower', back_populates='power')
-
 class HeroPower(db.Model):
     __tablename__ = 'hero_power'
 
     id = db.Column(db.Integer, primary_key=True)
     hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
     power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
-    strength = db.Column(db.String(50), nullable=True)  # Add validation later
+    strength = db.Column(db.String(10), nullable=False)
 
-    # Define the relationships with Hero and Power
-    hero = db.relationship('Hero', back_populates='powers')
-    power = db.relationship('Power', back_populates='heroes')
+    @validates('strength')
+    def validate_strength(self, key, strength):
+        allowed_strengths = ["Strong", "Weak", "Average"]
+        if strength not in allowed_strengths:
+            raise ValueError("Strength must be one of 'Strong', 'Weak', 'Average'")
+        return strength
--- code-challenge/app/seed.py
+++ code-challenge/app/seed.py
@@ -1,42 +1,43 @@
-from models import db, Hero, Power, HeroPower
-import random
+import random  # Import the random module
+from app import db, Hero, Power, HeroPower
 
 print("🦸‍♀️ Seeding powers...")
-powers_data = [
-    {"name": "super strength", "description": "gives the wielder super-human strengths"},
-    {"name": "flight", "description": "gives the wielder the ability to fly through the skies at supersonic speed"},
-    {"name": "super human senses", "description": "allows the wielder to use her senses at a super-human level"},
-    {"name": "elasticity", "description": "can stretch the human body to extreme lengths"}
+powers = [
+  { "name": "super strength", "description": "gives the wielder super-human strengths" },
+  { "name": "flight", "description": "gives the wielder the ability to fly through the skies at supersonic speed" },
+  { "name": "super human senses", "description": "allows the wielder to use her senses at a super-human level" },
+  { "name": "elasticity", "description": "can stretch the human body to extreme lengths" }
 ]
 
-for power_info in powers_data:
-    power = Power(**power_info)
+for power_data in powers:
+    power = Power(**power_data)
     db.session.add(power)
 
 print("🦸‍♀️ Seeding heroes...")
-heroes_data = [
-    {"name": "Kamala Khan", "super_name": "Ms. Marvel"},
-    {"name": "Doreen Green", "super_name": "Squirrel Girl"},
-    {"name": "Gwen Stacy", "super_name": "Spider-Gwen"},
-    {"name": "Janet Van Dyne", "super_name": "The Wasp"},
-    {"name": "Wanda Maximoff", "super_name": "Scarlet Witch"},
-    {"name": "Carol Danvers", "super_name": "Captain Marvel"},
-    {"name": "Jean Grey", "super_name": "Dark Phoenix"},
-    {"name": "Ororo Munroe", "super_name": "Storm"},
-    {"name": "Kitty Pryde", "super_name": "Shadowcat"},
-    {"name": "Elektra Natchios", "super_name": "Elektra"}
+heroes = [
+  { "name": "Kamala Khan", "super_name": "Ms. Marvel" },
+  { "name": "Doreen Green", "super_name": "Squirrel Girl" },
+  { "name": "Gwen Stacy", "super_name": "Spider-Gwen" },
+  { "name": "Janet Van Dyne", "super_name": "The Wasp" },
+  { "name": "Wanda Maximoff", "super_name": "Scarlet Witch" },
+  { "name": "Carol Danvers", "super_name": "Captain Marvel" },
+  { "name": "Jean Grey", "super_name": "Dark Phoenix" },
+  { "name": "Ororo Munroe", "super_name": "Storm" },
+  { "name": "Kitty Pryde", "super_name": "Shadowcat" },
+  { "name": "Elektra Natchios", "super_name": "Elektra" }
 ]
 
-for hero_info in heroes_data:
-    hero = Hero(**hero_info)
+for hero_data in heroes:
+    hero = Hero(**hero_data)
     db.session.add(hero)
 
 print("🦸‍♀️ Adding powers to heroes...")
 strengths = ["Strong", "Weak", "Average"]
+
 for hero in Hero.query.all():
-    for _ in range(random.randint(1, 3)):
-        power = random.choice(Power.query.all())
-        strength = random.choice(strengths)
+    for _ in range(1, 4):  # Randomly assign 1 to 3 powers to each hero
+        power = Power.query.get(random.randint(1, len(powers)))  # Fix here
+        strength = random.choice(strengths)  # Fix here
         hero_power = HeroPower(hero=hero, power=power, strength=strength)
         db.session.add(hero_power)
 
