1bcd6682eea311fa3677b664a6195ba62c3bcf65
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-10-02 09:56:35 +0300

    seeding heroes

--- code-challenge/db/seed.py
+++ code-challenge/db/seed.py
@@ -8,26 +8,26 @@ powers = [
   Power(name="super human senses", description="allows the wielder to use her senses at a super-human level"),
   Power(name="elasticity", description="can stretch the human body to extreme lengths")
 ]
+db.session.add_all(powers)
 
+print ("🦸‍♀️ Seeding heroes...")
+heroes= [
+  Hero(name="Kamala Khan", super_name="Ms. Marvel"),
+  Hero(name="Doreen Green", super_name="Squirrel Girl"),
+  Hero(name="Gwen Stacy", super_name="Spider-Gwen"),
+  Hero(name="Janet Van Dyne", super_name="The Wasp"),
+  Hero(name="Wanda Maximoff", super_name="Scarlet Witch"),
+  Hero(name="Carol Danvers", super_name="Captain Marvel"),
+  Hero(name="Jean Grey", super_name="Dark Phoenix"),
+  Hero(name="Ororo Munroe", super_name="Storm"),
+  Hero(name="Kitty Pryde", super_name="Shadowcat"),
+  Hero(name="Elektra Natchios", super_name="Elektra")
+]
+db.session.add_all(heroes)
 
-
-puts "🦸‍♀️ Seeding heroes..."
-Hero.create([
-  { name: "Kamala Khan", super_name: "Ms. Marvel" },
-  { name: "Doreen Green", super_name: "Squirrel Girl" },
-  { name: "Gwen Stacy", super_name: "Spider-Gwen" },
-  { name: "Janet Van Dyne", super_name: "The Wasp" },
-  { name: "Wanda Maximoff", super_name: "Scarlet Witch" },
-  { name: "Carol Danvers", super_name: "Captain Marvel" },
-  { name: "Jean Grey", super_name: "Dark Phoenix" },
-  { name: "Ororo Munroe", super_name: "Storm" },
-  { name: "Kitty Pryde", super_name: "Shadowcat" },
-  { name: "Elektra Natchios", super_name: "Elektra" }
-])
-
-puts "🦸‍♀️ Adding powers to heroes..."
-
+print("🦸‍♀️ Adding powers to heroes...")
 strengths = ["Strong", "Weak", "Average"]
+
 Hero.all.each do |hero|
   rand(1..3).times do
     # get a random power
